5278e640e5e6942dbfb058e6030c60d81928f90d
Reuben Aronson <rmaronson@cmu.edu>
2020-03-11 16:39:02 -0400

    get pupil conn working

--- src/ada_meal_scenario/gaze_interface_selector.py
+++ src/ada_meal_scenario/gaze_interface_selector.py
@@ -3,9 +3,24 @@
 import Tkinter
 import ttk
 import tkFont
+import gazetracking.pupil_capture as pupil
 
 class PupilConnection:
-    pass
+    def __init__(self):
+        self.connection = None
+        
+    def update(self, endpoint):
+        try:
+            self.connection = pupil.ConfigurablePupilCapture(endpoint)
+            return True, 'Connected'
+        except Exception as e:
+            return False, str(e)
+    
+    def get_connection(self):
+        if self.connection is None:
+            raise ValueError('Failed to configure pupil connection!')
+        return self.connection
+        
 
 class TobiiConnection:
     pass
@@ -23,6 +38,7 @@ class GazeInterfaceSelector:
         # Pupil info
         self.pupil_frame = Tkinter.Frame(frame)
         self.pupil_frame.grid(row=2, column=1)
+        self.pupil_connection = PupilConnection()
         
         # Pupil endpoint
         self.label_pupil_endpoint = Tkinter.Label(self.pupil_frame, text="Pupil endpoint")
@@ -33,6 +49,7 @@ class GazeInterfaceSelector:
         self.gaze_conn_info_elems['pupil'] = [self.entry_pupil_endpoint]
         self.entry_pupil_endpoint['state'] = 'disabled'
         self.value_pupil_endpoint.set("tcp://127.0.0.1:50020")
+        self.entry_pupil_endpoint.bind("<Return>", self.update_pupil_endpoint)
         
         # Pupil status
         self.label_pupil_status = Tkinter.Label(self.pupil_frame, text="Not connected", font=self.status_font)
@@ -97,8 +114,13 @@ class GazeInterfaceSelector:
 
         self.gaze_conn_info_elems['tobii'] = [self.entry_tobii_endpoint, self.combobox_tobii_project]
     
+    def update_pupil_endpoint(self, evt):
+        self.label_pupil_status['text'] = 'Trying to connect...'
+        self.label_pupil_status.update_idletasks()
+        _, msg = self.pupil_connection.update(self.value_pupil_endpoint.get())
+        self.label_pupil_status['text'] = msg
+    
     def update_tobii_endpoint(self, evt):
-        print('Called update')
         if self.entry_tobii_endpoint['state'] == 'normal':
             # try to connect with Tobii to list projects.....
             self.combobox_tobii_project['state'] = 'normal'
