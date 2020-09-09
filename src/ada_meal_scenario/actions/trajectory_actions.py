e8eab59af4e56ea35ca67343e3b439938577a794
Reuben Aronson <rmaronson@cmu.edu>
2020-09-09 14:53:10 -0400

    better traj loading stuff

--- src/ada_meal_scenario/actions/trajectory_actions.py
+++ src/ada_meal_scenario/actions/trajectory_actions.py
@@ -1,3 +1,5 @@
+
+from catkin.find_in_workspaces import find_in_workspaces
 import logging, numpy, prpy, os
 import prpy.rave, prpy.util
 from action_sequence import ActionSequence, defer, futurize, NoOp
@@ -9,11 +11,11 @@ logger = logging.getLogger(project_name)
 
 
 class RunTrajectory(ActionSequence):
+    _TRAJ_PATH = None
     __CACHED_TRAJ__ = {}
-    def __init__(self, name, traj_path, traj_filename_base, prev_result=None, config=None):
+    def __init__(self, file_base, prev_result=None, config=None):
         # load up the trajectories
-        traj_filename = os.path.join(
-            traj_path, 'trajectories', config['robot'].GetName() + '_' + traj_filename_base)
+        traj_filename = _get_traj_file(file_base, config)
         if traj_filename not in RunTrajectory.__CACHED_TRAJ__:
             RunTrajectory.__CACHED_TRAJ__[
                 traj_filename] = prpy.rave.load_trajectory(config['env'], traj_filename)
@@ -28,19 +30,17 @@ class RunTrajectory(ActionSequence):
             action_factories = [self._bypass]
 
         super(RunTrajectory, self).__init__(action_factories=action_factories)
-        
 
     def _move_to_traj_start(self, prev_result, config):
-        cspec = self.traj.GetConfigurationSpecification()
-        first_wpt = self.traj.GetWaypoint(0)
-        first_config = cspec.ExtractJointValues(
-            first_wpt, self.robot, self.robot.GetActiveManipulator().GetArmIndices())
-        current_config = self.robot.GetActiveManipulator().GetDOFValues()
         if prpy.util.IsAtTrajectoryStart(self.robot, self.traj):
             return NoOp()
         else:
-            traj = self.robot.PostProcessPath(self.robot.PlanToConfiguration(first_config, defer=True))
-            return self._execute_traj(traj)
+            cspec = self.traj.GetConfigurationSpecification()
+            first_wpt = self.traj.GetWaypoint(0)
+            first_config = cspec.ExtractJointValues(
+                first_wpt, self.robot, self.robot.GetActiveManipulator().GetArmIndices())
+            traj_to_start = self.robot.PostProcessPath(self.robot.PlanToConfiguration(first_config, defer=True))
+            return self._execute_traj(traj_to_start)
 
     def _execute_traj(self, traj):
         if self.robot.simulated:
@@ -76,18 +76,23 @@ class RunTrajectory(ActionSequence):
         
 
 # load in the expected configurations
-from catkin.find_in_workspaces import find_in_workspaces
-traj_path = find_in_workspaces(
-    search_dirs=['share'],
-    project=project_name,
-    path='data',
-    first_match_only=True)
-if len(traj_path) > 0:
-    LookAtPlate = partial(RunTrajectory, 'LOOKING_AT_PLATE',
-                        traj_path[0], 'traj_lookingAtPlate.xml')
-    Serve = partial(RunTrajectory, 'SERVING', traj_path[0], 'traj_serving.xml')
-else:
-    logger.warning('Failed to find trajectory data path')
+def _get_traj_file(file_base, config):
+    if RunTrajectory._TRAJ_PATH is None:
+        traj_path = find_in_workspaces(
+            search_dirs=['share'],
+            project=project_name,
+            path='data',
+            first_match_only=True)
+        if not traj_path:
+            raise RuntimeError('Failed to find saved trajectory files')
+        RunTrajectory._TRAJ_PATH = traj_path[0]
+    
+    return os.path.join(RunTrajectory._TRAJ_PATH, 'trajectories', '{}_{}'.format(config['robot'].GetName(), file_base))
+    
+
+LookAtPlate = partial(RunTrajectory, 'traj_lookingAtPlate.xml')
+Serve = partial(RunTrajectory, 'traj_serving.xml')
+
 
 
 
